3a8dca56d1189e51746c9883b3bc581a591b88be
Samuel Brigha <sam.brigham@yale.edu>
2020-05-15 18:46:16 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(name_hash)
 lowest_key = nil
 lowest_value = Float::INFINITY
-hash.each do |key, value|
+name_hash.each do |key, value|
   if value < lowest_value
     lowest_value = value
     lowest_key = key
