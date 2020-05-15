6d439c2441fdbb72cd24e3017ef01aa712c83148
Samuel Brigha <sam.brigham@yale.edu>
2020-05-15 18:39:12 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,9 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+count = 0
 min_value_key = ""
+while count < 
 name_hash.each do |key, value|
   if 
     
